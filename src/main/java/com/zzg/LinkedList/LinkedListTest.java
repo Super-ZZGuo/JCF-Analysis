a90db0b96a46682b41dc5cf4a0572b307aa5f420
gogoing-code <1378412050@qq.com>
2021-11-20 15:44:58 +0800

    LinkedList提交

---
+++ src/main/java/com/zzg/LinkedList/LinkedListTest.java
@@ -0,0 +1,22 @@
+package com.zzg.LinkedList;/*
+@date 2021/11/18 - 4:05 下午
+*/
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+
+/**
+ * @author zzg
+ */
+public class LinkedListTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void LinkedListTest1(){
+        LinkedList<Integer> list = new LinkedList<>();
+
+    }
+}

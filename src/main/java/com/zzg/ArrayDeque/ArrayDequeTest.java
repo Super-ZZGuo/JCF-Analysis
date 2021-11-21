baf2c8875ae4253d7606ecf79e6b89bcf05c4770
gogoing-code <1378412050@qq.com>
2021-11-21 20:07:17 +0800

    [添加]-(ArrayDeque)

---
+++ src/main/java/com/zzg/ArrayDeque/ArrayDequeTest.java
@@ -0,0 +1,16 @@
+package com.zzg.ArrayDeque;/*
+@date 2021/11/21 - 3:48 下午
+*/
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class ArrayDequeTest {
+    public static void main(String[] args) {
+
+
+    }
+    public void ArrayDequeTest2(){
+        ArrayDeque<Integer> arr = new ArrayDeque<>();
+    }
+}

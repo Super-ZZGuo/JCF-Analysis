f3cb7a022850ae3be5798c96c517a3519edb58af
gogoing-code <1378412050@qq.com>
2021-11-23 23:08:57 +0800

    [添加](未完)-(String)

---
+++ src/main/java/com/zzg/String/StringTest.java
@@ -0,0 +1,18 @@
+package com.zzg.String;/*
+@date 2021/11/23 - 11:03 下午
+*/
+
+import org.junit.Test;
+
+public class StringTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void StringTest2(){
+        String str = new String();
+        StringBuffer sbf = new StringBuffer();
+        StringBuilder sbd = new StringBuilder();
+    }
+}

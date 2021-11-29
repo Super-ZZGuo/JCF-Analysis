2c9a9ed7dce6a52728443ae516ce685529b2c0c9
gogoing-code <1378412050@qq.com>
2021-11-29 13:19:44 +0800

    [新增](ing)-(PriorityQueue init)

---
+++ src/main/java/com/zzg/PriorityQueue/PriorityQueueTest.java
@@ -0,0 +1,16 @@
+package com.zzg.PriorityQueue;/*
+@date 2021/11/29 - 1:18 下午
+*/
+
+import org.junit.Test;
+
+public class PriorityQueueTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void PriorityTest2(){
+
+    }
+}

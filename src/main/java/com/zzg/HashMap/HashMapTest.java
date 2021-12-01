dc4f21df3e34591e7b417704b138510bc2e24002
gogoing-code <1378412050@qq.com>
2021-12-01 15:59:51 +0800

    ✍️[新增](done)-(HashMap)

---
+++ src/main/java/com/zzg/HashMap/HashMapTest.java
@@ -0,0 +1,20 @@
+package com.zzg.HashMap;/*
+@date 2021/11/30 - 9:18 下午
+*/
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void HashMapTest2(){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
+    }
+}

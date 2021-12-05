f5eb06195c7b5b3da10aab7dea0b4975bb398f70
gogoing-code <1378412050@qq.com>
2021-12-05 16:42:21 +0800

    ✍️[新增](done)-(LinkedHashMap)

---
+++ src/main/java/com/zzg/LinkedHashMap/LinkedHashMapTest.java
@@ -0,0 +1,16 @@
+package com.zzg.LinkedHashMap;/*
+@date 2021/12/5 - 11:03 上午
+*/
+
+import org.junit.Test;
+
+import java.util.LinkedHashMap;
+
+public class LinkedHashMapTest {
+
+    @Test
+    public void LinkedHashMapTest(){
+        LinkedHashMap<Integer,Integer> map = new LinkedHashMap<>();
+    }
+
+}
